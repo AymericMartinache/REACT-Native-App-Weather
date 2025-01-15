ccd856b816698f19068e1c58452934808ec49967
Aymeric M <aymeric.martinache@gmail.com>
2025-01-15 09:45:13 +0100

    Changing logic and adding api call

--- App.js
+++ App.js
@@ -1,9 +1,5 @@
 // EXPO
 import { StatusBar } from 'expo-status-bar';
-import {
-    requestForegroundPermissionsAsync,
-    getCurrentPositionAsync,
-} from 'expo-location';
 
 // REACT NATIVE
 import { ImageBackground, Text, View } from 'react-native';
@@ -17,36 +13,8 @@ import background from './assets/img/background-app.png';
 
 // COMPONENTS
 import Home from './pages/Home/Home';
-import { useEffect, useState } from 'react';
 
 export default function App() {
-    // STATES
-    const [coords, setCoords] = useState();
-
-    // USE EFFECT
-    useEffect(() => {
-        getUserCoord();
-    }, []);
-
-    // Récupéraion des coordonnées GPS
-    async function getUserCoord() {
-        let { status } = await requestForegroundPermissionsAsync();
-        if (status === 'granted') {
-            const location = await getCurrentPositionAsync();
-            setCoords({
-                lat: location.coords.latitude,
-                lng: location.coords.longitude,
-            });
-        } else {
-            setCoords({
-                lat: '48.8555555',
-                lng: '2,35',
-            });
-        }
-        console.log('Permission :', status);
-    }
-    console.log('Location : ', coords);
-
     return (
         <ImageBackground
             source={background}
--- pages/Home/Home.jsx
+++ pages/Home/Home.jsx
@@ -1,13 +1,65 @@
 // REACT NATIVE
 import { Text, View } from 'react-native';
 
+// EXPO
+import {
+    requestForegroundPermissionsAsync,
+    getCurrentPositionAsync,
+} from 'expo-location';
+
 // STYLES
 import { styles } from './Home.style';
 
 // COMPONENTS
 import Meteo_basic from '../../components/Meteo_basic/Meteo_basic';
 
+// REACT
+import { useEffect, useState } from 'react';
+
 export default function Home() {
+    // STATES
+    const [coords, setCoords] = useState();
+
+    // USE EFFECT
+    // Récupérations des coordonnées
+    useEffect(() => {
+        getUserCoord();
+    }, []);
+
+    useEffect(() => {
+        fetchWeather();
+    }, [coords]);
+
+    // Récupéraion des coordonnées GPS
+    async function getUserCoord() {
+        let { status } = await requestForegroundPermissionsAsync();
+        // On vérifie que l'utilisateur accèpte la géoloc
+        if (status === 'granted') {
+            const location = await getCurrentPositionAsync();
+            setCoords({
+                lat: location.coords.latitude,
+                lng: location.coords.longitude,
+            });
+
+            // Sinon on ajoute des valeurs par defaut
+        } else {
+            setCoords({
+                lat: '48.8555555',
+                lng: '2,35',
+            });
+        }
+
+        console.log('Permission :', status);
+    }
+
+    console.log('Location : ', coords);
+
+    // Fetch de la météo
+    async function fetchWeather(coordinates) {
+        // fetch
+        console.log('FETCH WEATHER');
+    }
+
     return (
         <View style={styles.container}>
             <Meteo_basic />
